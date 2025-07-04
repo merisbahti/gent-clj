a4117e85835df4712367ba9e9c3dee49308dc7d8
meris <meris.bahti@gmail.com>
2025-07-04 06:19:44 +0200

    project settings

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject gent-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "(a)gent"
+  :url "git@github.com:merisbahti/gent-clj.git"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0",
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
